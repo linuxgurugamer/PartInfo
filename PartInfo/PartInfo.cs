1d508b83c43349881eb313e15775df75153ebe76
Jonathan Bayer <linuxgurugamer@gmail.com>
2019-02-07 14:44:16 -0500

    Initial commit

---
+++ PartInfo/PartInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PartInfo
+{
+    public class ModulePartInfo : PartModule
+    {
+        public override string GetInfo()
+        {
+
+            string st = "Part Name: " + this.part.partInfo.name + "\n" + "Part Path: " + this.part.partInfo.partUrl;
+            return st;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+PartInfo is a simple mod to expose the internal part name and path to the part file.  
+
+The data shows up in the VAB/SPH when you right-click on a part in the part list
